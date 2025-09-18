f4d8a0f17712cd29ee6b32e3b3f878830c761dc0
GabrielAcker <gabriel.acker@gmail.com>
2025-09-18 11:25:22 -0300

    atualizar Gabriel

--- Jobs/JallCardFetchJob.php
+++ Jobs/JallCardFetchJob.php
@@ -28,6 +28,7 @@ class JallCardFetchJob
         try {
             // 1. Coletar dados da JallCard (últimos 7 dias)
             $pedidosJallCardRaw = JallCardHelper::getArquivosProcessadosUltimos7Dias();
+            LogHelper::logBitrixHelpers("Dados brutos da JallCard (últimos 7 dias): " . json_encode($pedidosJallCardRaw), __CLASS__ . '::' . __FUNCTION__);
             
             if (empty($pedidosJallCardRaw)) {
                 LogHelper::logBitrixHelpers("Nenhum arquivo processado encontrado na JallCard nos últimos 7 dias.", __CLASS__ . '::' . __FUNCTION__);
@@ -37,15 +38,20 @@ class JallCardFetchJob
             // 2. Processar cada pedido da JallCard e salvar/atualizar na tabela vinculacao_jallcard
             foreach ($pedidosJallCardRaw as $pedidoJallCardItem) {
                 $pedidoProducaoJallCard = $pedidoJallCardItem['pedidoProducao'];
+                LogHelper::logBitrixHelpers("Processando PedidoProducao: {$pedidoProducaoJallCard}", __CLASS__ . '::' . __FUNCTION__);
 
                 // Verificar se este pedidoProducao já foi processado na tabela temporária
-                if ($this->databaseRepository->findVinculacaoJallCardByPedidoProducao($pedidoProducaoJallCard)) {
+                $vinculacaoExistente = $this->databaseRepository->findVinculacaoJallCardByPedidoProducao($pedidoProducaoJallCard);
+                LogHelper::logBitrixHelpers("Resultado da busca por vinculação existente para {$pedidoProducaoJallCard}: " . json_encode($vinculacaoExistente), __CLASS__ . '::' . __FUNCTION__);
+
+                if ($vinculacaoExistente) {
                     LogHelper::logBitrixHelpers("PedidoProducao {$pedidoProducaoJallCard} já processado na tabela temporária. Ignorando.", __CLASS__ . '::' . __FUNCTION__);
                     continue;
                 }
 
                 // Obter detalhes do pedido (OP e nomes de arquivos)
                 $detalhesPedido = JallCardHelper::getPedidoProducao($pedidoProducaoJallCard);
+                LogHelper::logBitrixHelpers("Detalhes do pedido para {$pedidoProducaoJallCard}: " . json_encode($detalhesPedido), __CLASS__ . '::' . __FUNCTION__);
 
                 if (!$detalhesPedido || empty($detalhesPedido['ops'])) {
                     LogHelper::logBitrixHelpers("Não foi possível obter detalhes ou OP para PedidoProducao {$pedidoProducaoJallCard}.", __CLASS__ . '::' . __FUNCTION__);
@@ -64,16 +70,18 @@ class JallCardFetchJob
                         $nomeArquivoConvertido = $arquivo['nome'];
                     }
                 }
+                LogHelper::logBitrixHelpers("OP: {$opJallCard}, Arquivo Original: {$nomeArquivoOriginal}, Arquivo Convertido: {$nomeArquivoConvertido} para PedidoProducao {$pedidoProducaoJallCard}.", __CLASS__ . '::' . __FUNCTION__);
 
                 // Inserir na tabela temporária
-                $this->databaseRepository->inserirVinculacaoJallCard([
+                $dadosParaInserir = [
                     'pedido_producao_jallcard' => $pedidoProducaoJallCard,
                     'op_jallcard' => $opJallCard,
                     'nome_arquivo_original_jallcard' => $nomeArquivoOriginal,
                     'nome_arquivo_convertido_jallcard' => $nomeArquivoConvertido,
                     'data_processamento_jallcard' => $pedidoJallCardItem['dataProcessamento']
-                ]);
-                LogHelper::logBitrixHelpers("PedidoProducao {$pedidoProducaoJallCard} inserido na tabela vinculacao_jallcard.", __CLASS__ . '::' . __FUNCTION__);
+                ];
+                $this->databaseRepository->inserirVinculacaoJallCard($dadosParaInserir);
+                LogHelper::logBitrixHelpers("Dados inseridos na tabela vinculacao_jallcard para PedidoProducao {$pedidoProducaoJallCard}: " . json_encode($dadosParaInserir), __CLASS__ . '::' . __FUNCTION__);
             }
 
             LogHelper::logBitrixHelpers("JallCardFetchJob finalizado.", __CLASS__ . '::' . __FUNCTION__);
